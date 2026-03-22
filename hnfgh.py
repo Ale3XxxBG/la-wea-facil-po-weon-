29f457f1b7aa3c199f454f69eafbbbc95af47881
al3XxBg <elpromaster69.mx@gmail.com>
2026-03-22 17:53:18 -0600

    Add files via upload

---
+++ hnfgh.py
@@ -0,0 +1,12 @@
+nombre_astronauta = "Alex"
+edad_astronauta = 67
+destino = "Marte"
+
+print("diario de un astronauta\n")
+
+print(f"Hola, soy {nombre_astronauta}, tengo {edad_astronauta} años y mi próximo destino es {destino}.\n")
+
+combustible = 67
+velocidad = 67000
+
+print(f"Estoy navegando a {velocidad}km/s con {combustible}% de combustible restante hacia {destino}.\n")
